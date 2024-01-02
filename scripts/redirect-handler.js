473a51494e70946b88f8bb983ba03709a3d2b438
Nikki <coinslotgaming@gmail.com>
2024-01-02 03:57:25 -0500

    Updates

--- scripts/redirect-handler.js
+++
@@ -1,9 +0,0 @@
-function modifyURL() {
-        if(window.location.href.endsWith("/index.html"))
-                window.location.href = window.location.href.replace("/index.html", "");
-        else if(window.location.hostname === "github.com")
-        window.location.href = window.location.href.replace(".html", "");
-}
-
-// I don't wanna wait for this ik it's bad practice
-modifyURL();
