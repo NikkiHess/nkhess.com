faefbce152ca19cd008146c4e479d16537c06531
Nikki <coinslotgaming@gmail.com>
2024-01-09 03:43:26 -0500

    Continued styling home page
    Need to finish scaling text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,16 @@ export const metadata: Metadata = {
   description: 'Nikki Hess\'s portfolio site homepage!',
 }
 
+// TODO: Investigate weird glitch at width 465
+
 const Home = () => (
-  <div id={indexStyles.homeContent}>
+  <div id={indexStyles.homeContainer}>
     <div id={indexStyles.innerHome}>
-      <img src='images/christmas-small.png' alt='A photo of Nikki' id={indexStyles.nikkiPikki}></img>
+      <div id={indexStyles.imgContainer}>
+        <img src='images/christmas-small.png' alt='A photo of Nikki' id={indexStyles.nikkiPikki}></img>
+        <div id={indexStyles.topLeftStar}>★</div>
+        <div id={indexStyles.bottomRightStar}>★</div>
+      </div>
       <p id={indexStyles.about}>
         Hello, my name is <b>Nikki Hess</b>! 
       </p>
@@ -28,16 +34,6 @@ const Home = () => (
           Working on learning <b> Next.js </b>
         </li>
       </ul>
-      {/* <p>
-        I'm a senior at the University of Michigan 
-        studying <b>computer science</b>! I'm an <b>aspiring game developer</b> and <b>software 
-        engineer</b> with a focus on back-end development.
-      </p>
-      <p>
-        I am currently trying to learn front-end development. This version of my website
-        is made with <b>Next.js</b>! I'm pretty proud of how it turned out!
-      </p> */}
-      {/* <h1> Current Projects </h1> */}
     </div>
   </div>
 );
