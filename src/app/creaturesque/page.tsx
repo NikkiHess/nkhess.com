9ab923b9e7d2701382b60a852bdcd8321a5195dc
Nikki <coinslotgaming@gmail.com>
2025-02-04 21:32:08 -0500

    :lipstick: resume updates

---
+++ src/app/creaturesque/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Creaturesque',
+    description: 'Play Creaturesque!',
+}
+
+const Creaturesque = () => (
+  <div>
+    
+  </div>
+);
+
+export default Creaturesque;
